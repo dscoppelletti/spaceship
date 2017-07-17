41b1090d97e899d5e337c4ec266117dd8e493fbb
Dario Scoppelletti <dario@scoppelletti.it>
2017-07-17 11:03:09 +0200

    Implemented CSRNG initialization

---
+++ lib/src/main/java/it/scoppelletti/spaceship/security/SecurityExt.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2008-2017 Dario Scoppelletti, <http://www.scoppelletti.it/>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package it.scoppelletti.spaceship.security;
+
+import java.security.SecureRandom;
+import java.util.Random;
+import android.support.annotation.NonNull;
+
+/**
+ * Operations for security.
+ *
+ * @since 1.0.0
+ */
+public final class SecurityExt {
+
+    /**
+     * Private constructor for static class.
+     */
+    private SecurityExt() {
+    }
+
+    /**
+     * Gets a cryptographically strong random number generator.
+     *
+     * @return The object.
+     */
+    @NonNull
+    public static Random getCSRNG() {
+        return SecurityExt.SecureRandomHolder.myRNG;
+    }
+
+    /**
+     * On-demand initialization that does not need synchronization.
+     */
+    private static final class SecureRandomHolder {
+        static final Random myRNG = new SecureRandom();
+    }
+}
